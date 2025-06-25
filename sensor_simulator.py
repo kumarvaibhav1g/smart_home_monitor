62e943a21197ff3fec20851a573828bcb896124c
Kumar Vaibhav G <kumarvaibhavg2002@gmail.com>
2025-06-25 21:18:28 +0530

    Update sensor_simulator.py

--- sensor_simulator.py
+++ sensor_simulator.py
@@ -1,6 +1,9 @@
 import requests
 import random
 import time
+import os
+
+TARGET_URL = os.getenv("TARGET_URL", "http://127.0.0.1:5000/sensor")
 
 while True:
     temp = round(random.uniform(20, 30), 2)
@@ -8,7 +11,7 @@ while True:
     data = {"temperature": temp, "humidity": humidity}
 
     try:
-        response = requests.post("https://smart-home-monitor.onrender.com", json=data)
+        response = requests.post(TARGET_URL, json=data)
         print(f"Sent: {data}, Status: {response.status_code}")
     except Exception as e:
         print(f"Failed to send: {e}")
